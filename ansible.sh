41df0e1ebcd12aa2bd49e4c7b2437709b8a5357d
Ian Allison <iana@pims.math.ca>
2016-11-21 15:21:21 -0800

    Adding branch for ansible to use
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ASSETS="CNAME
+ASSETLIST="CNAME
 LICENSE
 README.md
 _config.yml
@@ -17,7 +17,15 @@ index.html
 js
 logout"
 
-for asset in $ASSETS ; do
-    rm -rf $asset
+BRANCHES=`git branch | grep -E '^\*' | cut -d' ' -f 2`
+if [[ $BRANCHES =~ ^ansible$ ]] ; then
+    echo "On Branch ansible"
+    for asset in $ASSETLIST ; do
+        echo $asset
+        rm -rf $asset
+    done
     mv _site site
-done
+else
+    echo "Not on ansible branch"
+    exit 1
+fi
