7c256783a0660944bd60aeb4668a0791a4fdb2ed
Ian Allison <iana@pims.math.ca>
2021-02-02 18:20:28 -0800

    Minor shell tweak
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- customize.sh
+++ customize.sh
@@ -12,7 +12,18 @@ UNIVNAME=$2
 FILES=('index.html' 'index.xml' 'tags/index.xml' 'categories/index.xml')
 
 for file in ${FILES[@]}; do
-    echo "Processing $file"
+    if [ ! -f ${file} ] ; then
+        echo "Unable to find ${file}"
+        exit 1
+    fi
     sed "s/XXXSITENAME/${SITENAME}/g" $file
+    if [ $? -ne 0 ] ; then
+        echo "Unable to process SITENAME for find ${file}"
+        exit 1
+    fi
     sed "s/XXXUNIVNAME/${UNIVNAME}/g" $file
+    if [ $? -ne 0 ] ; then
+        echo "Unable to process UNIVNAME for find ${file}"
+        exit 1
+    fi
 done
